fde732b234e26eff5e2cb6c092bc04873edcea88
mofas <fedt1234@hotmail.com>
2014-05-19 15:36:05 +0800

    enhance patmat test case

--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -13,6 +13,11 @@ class HuffmanSuite extends FunSuite {
     val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
     val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
   }
+  
+  test("times function") {    
+    assert(times(List('a', 'b', 'a')) === List(('a', 2), ('b', 1)))     
+    assert(times(List('a', 'b', 'c', 'a', 'b')) === List(('a',2), ('b',2), ('c',1)))    
+  }
 
   test("weight of a larger tree") {
     new TestTrees {
@@ -33,6 +38,19 @@ class HuffmanSuite extends FunSuite {
   test("makeOrderedLeafList for some frequency table") {
     assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
   }
+      
+  test("until function") {
+    val treesList = until(singleton, combine)(combine(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3)))))
+    assert(weight(treesList(0)) === 6)
+    assert(chars(treesList(0)) === List('e', 't', 'x'))
+    
+    val treesList2 = createCodeTree(List('a','c','b','c','b','b','a','a','d','a'))
+    assert(weight(treesList2) === 10)
+	assert(chars(treesList2).contains('a'))
+	assert(chars(treesList2).contains('b'))
+	assert(chars(treesList2).contains('c'))
+	assert(chars(treesList2).contains('d'))
+  }
 
   test("combine of some leaf list") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
@@ -43,5 +61,25 @@ class HuffmanSuite extends FunSuite {
     new TestTrees {
       assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
     }
+  }
+    	  
+  test("decode and encode") {
+    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+	assert(decode(t1, encode(t1)("ab".toList)) === List('a', 'b')) 
+	assert(decode(t1, encode(t1)("baaab".toList)) === List('b', 'a', 'a', 'a', 'b'))
+  }
+  
+  test("convert function") {
+	assert(convert(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)) === List(('a',List(0)), ('b',List(1))))
+  }
+    
+  test("quickEncode function") {
+	val t2 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+	assert(decode(t2, quickEncode(t2)("ab".toList)) === List('a', 'b'))
+	assert(decode(t2, quickEncode(t2)("baaab".toList)) === List('b', 'a', 'a', 'a', 'b'))
+  }
+
+  test("decodedSecret function") {
+	  assert(decodedSecret === List('h', 'u', 'f', 'f', 'm', 'a', 'n', 'e', 's', 't', 'c', 'o', 'o', 'l'))
   }
 }
